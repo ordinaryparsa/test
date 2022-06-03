c5d91982281b7cc293f375bf8991c6554bf2cbdc
another parsa <anotherparsa@gmail.com>
2022-06-03 20:44:24 +0430

    registration part has been added

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, redirect, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
+
+#connecting to database:
+db = mysql.connector.connect(
+    host = "127.0.0.1",
+    user = "root",
+    password = "I@mth3p4r34"
+)
+
+#defining cursor:
+mycursor = db.cursor()
 
 @app.route("/")
 def home():
     return render_template("home/home.html")
 
+#registration part:
+#registraiton page:
+@app.route("/registration")
+def registration():
+    return render_template("registration/registration.html")
 
 
-
-
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if len(username) <= 10 and len(password) <= 12:
+        mycursor.execute("use users")
+        mycursor.execute(f"SELECT * FROM user WHERE username='\{username}\' ")
+        userExist = mycursor.fetchall()
+        rule = "user"
+        if userExist:
+            return "sorry the username is already exist"
+        else:
+            mycursor.execute("use users")
+            mycursor.execute(f"INSERT INTO user (username, password, rule) VALUES (\'{username}\', \'{password}\', \'{rule}\')")
+            db.commit()
+            return redirect("/")
+    else:
+        return "your username doesn't have the requeirenments"
 
 
 
