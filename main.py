c2ffac3c4cd8c80e2298851e8db53915a08bf9ec
another parsa <anotherparsa@gmail.com>
2022-06-07 14:51:22 +0430

    added 404 page

--- main.py
+++ main.py
@@ -160,5 +160,12 @@ def userdelete(userDelete):
     else:
         return "sorry you're not authorized for this"
 
+#404 page
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404/404.html"), 404
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
